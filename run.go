65d7cd3bcd7a0c3cb3d613b3175e71b2623bd82a
Evan Byrne <evantbyrne@gmail.com>
2024-05-25 12:26:57 -0400

    Run one-off commands on networks

--- run.go
+++ run.go
@@ -14,6 +14,8 @@ type RunCommand struct {
 	Image       []string `arg:"" name:"image" passthrough:"" help:"Docker image."`
 
 	ConfigFile string `flag:"" name:"config" help:"Config file." type:"path" default:".rove"`
+	Network    string `flag:"" name:"network" help:"Network name." default:""`
+	Prefix     string `flag:"" name:"prefix" help:"Network prefix." default:"rove."`
 }
 
 func (cmd *RunCommand) Run() error {
@@ -33,8 +35,13 @@ func (cmd *RunCommand) Run() error {
 			return fmt.Errorf("unable to read private key file: %v", err)
 		}
 		return SshConnect(fmt.Sprintf("%s:%d", machine.Address, machine.Port), machine.User, key, func(conn *SshConnection) error {
+			command := "docker run --label rove"
+			if cmd.Network != "" {
+				command = fmt.Sprint(command, " --network ", cmd.Prefix, cmd.Network)
+			}
+			command = fmt.Sprint(command, " ", strings.Join(cmd.Image, " "))
 			return conn.
-				Run(fmt.Sprintf("docker run %s", strings.Join(cmd.Image, " ")), func(res string) error {
+				Run(command, func(res string) error {
 					fmt.Print(res)
 					return nil
 				}).
