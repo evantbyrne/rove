8d739c8299776b781048767157880a873b486e4f
Evan Byrne <evantbyrne@gmail.com>
2024-06-08 16:28:56 -0400

    Machine add confirmation

--- machine_add.go
+++ machine_add.go
@@ -1,8 +1,10 @@
 package rove
 
 import (
+	"bufio"
 	"database/sql"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"strings"
@@ -26,6 +28,7 @@ type MachineAddCommand struct {
 	PrivateKeyFile string `arg:"" name:"pk" help:"Private key file." type:"path"`
 
 	ConfigFile string `flag:"" name:"config" help:"Config file." type:"path" default:".rove"`
+	Force      bool   `flag:"" name:"force" help:"Skip confirmations."`
 	Port       int64  `flag:"" name:"port" help:"SSH port of remote machine." default:"22"`
 	Prefix     string `flag:"" name:"prefix" help:"Network prefix." default:"rove."`
 }
@@ -45,8 +48,10 @@ func (cmd *MachineAddCommand) Run() error {
 			return fmt.Errorf("unable to read private key file: %v", err)
 		}
 		err = SshConnect(fmt.Sprintf("%s:%d", cmd.Address, cmd.Port), cmd.User, key, func(conn *SshConnection) error {
-			fmt.Printf("✅ Connected to remote address '%s@%s:%d'\n", cmd.User, cmd.Address, cmd.Port)
-			return conn.
+			fmt.Printf("\nConnected to remote address '%s@%s:%d'.\n", cmd.User, cmd.Address, cmd.Port)
+			mustEnableSwarm := true
+			mustCreateNetwork := true
+			err := conn.
 				Run("docker info --format json", func(res string) error {
 					var dockerInfo DockerInfoJson
 					if err := json.Unmarshal([]byte(res), &dockerInfo); err != nil {
@@ -54,29 +59,72 @@ func (cmd *MachineAddCommand) Run() error {
 						return err
 					}
 					if dockerInfo.Swarm.NodeID != "" {
-						fmt.Println("✅ Remote machine already part of a swarm")
-						return ErrorSkip{}
+						mustEnableSwarm = false
 					}
 					return nil
 				}).
-				Run(fmt.Sprintf("docker swarm init --advertise-addr %s", cmd.Address), func(_ string) error {
-					fmt.Println("✅ Enabled swarm on remote machine")
-					return nil
-				}).
-				OnError(SkipReset).
 				Run(fmt.Sprint("docker network ls --format json --filter label=rove --filter name=", cmd.Prefix, "default"), func(res string) error {
 					if lines := strings.Split(strings.ReplaceAll(res, "\r\n", "\n"), "\n"); len(lines) > 1 {
-						fmt.Println("✅ Default network already exists")
-						return ErrorSkip{}
+						mustCreateNetwork = false
 					}
 					return nil
 				}).
-				Run(fmt.Sprint("docker network create --attachable --label rove ", cmd.Prefix, "default"), func(res string) error {
-					fmt.Println("✅ Created default network")
-					return nil
-				}).
-				OnError(SkipReset).
 				Error
+			if err != nil {
+				return err
+			}
+
+			if mustEnableSwarm || mustCreateNetwork {
+				fmt.Print("\nRove will make the following changes to remote machine:\n\n")
+				if mustEnableSwarm {
+					fmt.Println(" ~ Enable swarm")
+				}
+				if mustCreateNetwork {
+					fmt.Println(" ~ Create 'default' network")
+				}
+
+				if cmd.Force {
+					fmt.Println("\nConfirmations skipped.")
+				} else {
+					fmt.Println("\nDo you want Rove to run this deployment?")
+					fmt.Println("  Type 'yes' to approve, or anything else to deny.")
+					fmt.Print("  Enter a value: ")
+					line, err := bufio.NewReader(os.Stdin).ReadString('\n')
+					if err != nil {
+						fmt.Println("🚫 Could not read from STDIN")
+						return err
+					}
+					if strings.ToLower(strings.TrimSpace(line)) != "yes" {
+						return errors.New("🚫 Deployment canceled because response did not match 'yes'")
+					}
+				}
+				fmt.Print("\n")
+			}
+
+			if mustEnableSwarm {
+				err = conn.
+					Run(fmt.Sprintf("docker swarm init --advertise-addr %s", cmd.Address), func(_ string) error {
+						fmt.Println("~ Enabled swarm")
+						return nil
+					}).
+					Error
+				if err != nil {
+					return err
+				}
+			}
+			if mustCreateNetwork {
+				err = conn.
+					Run(fmt.Sprint("docker network create --attachable --label rove ", cmd.Prefix, "default"), func(res string) error {
+						fmt.Println("~ Created 'default' network")
+						return nil
+					}).
+					OnError(SkipReset).
+					Error
+				if err != nil {
+					return err
+				}
+			}
+			return nil
 		})
 		if err != nil {
 			fmt.Println("🚫 Could not add machine")
@@ -91,16 +139,15 @@ func (cmd *MachineAddCommand) Run() error {
 				User:    cmd.User,
 			}).
 			Then(func(_ sql.Result, _ *Machine) error {
-				fmt.Printf("✅ Added machine '%s'\n", cmd.Name)
 				return SetPreference(DefaultMachine, cmd.Name).Error
+			}).
+			Then(func(_ sql.Result, _ *Machine) error {
+				fmt.Printf("\nSetup '%s' and set as default machine.\n\n", cmd.Name)
+				return nil
 			}).
 			OnError(func(err error) error {
 				fmt.Println("🚫 Could not add machine")
 				return err
-			}).
-			Then(func(_ sql.Result, _ *Machine) error {
-				fmt.Printf("✅ Set default machine to '%s'\n", cmd.Name)
-				return nil
 			}).
 			Error
 	})
