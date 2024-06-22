fc8967ff326081a18a7cdad12ccb7562d8673491
Evan Byrne <evantbyrne@gmail.com>
2024-06-22 13:41:12 -0400

    Test network delete

--- network_delete.go
+++ network_delete.go
@@ -15,25 +15,27 @@ type NetworkDeleteCommand struct {
 	Machine    string `flag:"" name:"machine" help:"Name of machine." default:""`
 }
 
+func (cmd *NetworkDeleteCommand) Do(conn SshRunner, stdin io.Reader) error {
+	fmt.Printf("\nRove will delete the '%s' network.\n", cmd.Name)
+	if err := confirmDeployment(cmd.Force, stdin); err != nil {
+		return err
+	}
+	return conn.
+		Run(fmt.Sprint("docker network rm ", shellescape.Quote(cmd.Name)), func(res string) error {
+			fmt.Printf("\nDeleted '%s' network.\n\n", cmd.Name)
+			return nil
+		}).
+		OnError(func(err error) error {
+			if err != nil {
+				fmt.Println("🚫 Could not delete network")
+			}
+			return err
+		}).
+		Error()
+}
+
 func (cmd *NetworkDeleteCommand) Run() error {
 	return Database(cmd.ConfigFile, func() error {
-		return SshMachineByName(cmd.Machine, func(conn SshRunner, stdin io.Reader) error {
-			fmt.Printf("\nRove will delete the '%s' network.\n", cmd.Name)
-			if err := confirmDeployment(cmd.Force, stdin); err != nil {
-				return err
-			}
-			return conn.
-				Run(fmt.Sprint("docker network rm ", shellescape.Quote(cmd.Name)), func(res string) error {
-					fmt.Printf("Deleted '%s' network.\n\n", cmd.Name)
-					return nil
-				}).
-				OnError(func(err error) error {
-					if err != nil {
-						fmt.Println("🚫 Could not delete network")
-					}
-					return err
-				}).
-				Error()
-		})
+		return SshMachineByName(cmd.Machine, cmd.Do)
 	})
 }
---
+++ network_delete_test.go
@@ -0,0 +1,37 @@
+package rove
+
+import (
+	"fmt"
+	"slices"
+	"strings"
+	"testing"
+)
+
+func TestNetworkDeleteCommand(t *testing.T) {
+	if err := testDatabase(func() error {
+		mock := &SshConnectionMock{}
+		expectedCmd := []string{"docker network rm foo"}
+		expected := fmt.Sprint(
+			"\nRove will delete the 'foo' network.\n\n",
+			"Confirmations skipped.\n\n",
+			"Deleted 'foo' network.\n\n")
+
+		capture(t).
+			Run(func() error {
+				cmd := &NetworkDeleteCommand{
+					Force:   true,
+					Machine: "default",
+					Name:    "foo",
+				}
+				return cmd.Do(mock, strings.NewReader(""))
+			}).
+			ExpectStdout(expected)
+
+		if !slices.Equal(mock.CommandsRun, expectedCmd) {
+			t.Errorf("'%#v' did not match expected.", mock.CommandsRun)
+		}
+		return nil
+	}); err != nil {
+		t.Fatal(err)
+	}
+}
