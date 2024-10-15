8bac012e6791e72aabedb85b31d7326aba83e7a4
Evan Byrne <evantbyrne@gmail.com>
2024-10-14 21:03:29 -0400

    Make inspect command output in diff style by default #10

--- inspect.go
+++ inspect.go
@@ -4,6 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"strings"
+	"time"
+
+	"github.com/alessio/shellescape"
 )
 
 type InspectCommand struct {
@@ -11,25 +15,118 @@ type InspectCommand struct {
 
 	ConfigFile string `flag:"" name:"config" help:"Config file." type:"path" default:".rove"`
 	Machine    string `flag:"" name:"machine" help:"Name of machine." default:""`
+	Json       bool   `flag:"" name:"json"`
 }
 
 func (cmd *InspectCommand) Do(conn SshRunner, stdin io.Reader) error {
 	return conn.
 		Run(fmt.Sprint("docker service inspect --format json ", cmd.Name), func(res string) error {
-			var dockerInspect []map[string]any
-			if err := json.Unmarshal([]byte(res), &dockerInspect); err != nil {
-				fmt.Println("🚫 Could not parse docker service inspect JSON:\n", res)
-				return err
+			if cmd.Json {
+				var dockerInspect []map[string]any
+				if err := json.Unmarshal([]byte(res), &dockerInspect); err != nil {
+					fmt.Println("🚫 Could not parse docker service inspect JSON:\n", res)
+					return err
+				}
+				if len(dockerInspect) < 1 {
+					return fmt.Errorf("empty docker service inspect JSON: %s", dockerInspect)
+				}
+				out, err := json.MarshalIndent(dockerInspect[0], "", "    ")
+				if err != nil {
+					fmt.Println("🚫 Could not format JSON:\n", dockerInspect[0])
+					return err
+				}
+				fmt.Println(string(out))
+			} else {
+				var dockerInspect []DockerServiceInspectJson
+				if err := json.Unmarshal([]byte(res), &dockerInspect); err != nil {
+					fmt.Println("🚫 Could not parse docker service inspect JSON:\n", res)
+					return err
+				}
+				old := &ServiceState{}
+				old.Env = dockerInspect[0].Spec.TaskTemplate.ContainerSpec.Env
+				// TODO: Mounts
+
+				// Networks
+				networksExisting := make([]string, 0)
+				networksExistingIds := make([]string, 0)
+				for _, network := range dockerInspect[0].Spec.TaskTemplate.Networks {
+					networksExistingIds = append(networksExistingIds, network.Target)
+				}
+				if len(networksExistingIds) > 0 {
+					commandNetworks := ShellCommand{
+						Name: "docker network ls --format json --no-trunc",
+					}
+					for _, networkId := range networksExistingIds {
+						commandNetworks.Flags = append(commandNetworks.Flags, ShellFlag{
+							Check: networkId != "",
+							Name:  "filter",
+							Value: shellescape.Quote("id=" + networkId),
+						})
+					}
+					errNetwork := conn.
+						Run(commandNetworks.String(), func(resNetworks string) error {
+							for _, line := range strings.Split(strings.ReplaceAll(resNetworks, "\r\n", "\n"), "\n") {
+								if line != "" {
+									var dockerNetworkLs DockerNetworkLsJson
+									if err := json.Unmarshal([]byte(line), &dockerNetworkLs); err != nil {
+										fmt.Println("🚫 Could not parse docker network ls JSON:\n", line)
+										return err
+									}
+									networksExisting = append(networksExisting, dockerNetworkLs.Name)
+								}
+							}
+							return nil
+						}).
+						Error()
+					if errNetwork != nil {
+						return errNetwork
+					}
+				}
+
+				// Ports
+				portsExisting := make([]string, 0)
+				for _, entry := range dockerInspect[0].Spec.EndpointSpec.Ports {
+					port := fmt.Sprintf("%d:%d", entry.TargetPort, entry.PublishedPort)
+					portsExisting = append(portsExisting, port)
+				}
+
+				// Secrets
+				secretsExisting := make([]string, 0)
+				for _, secret := range dockerInspect[0].Spec.TaskTemplate.ContainerSpec.Secrets {
+					secretsExisting = append(secretsExisting, secret.SecretName)
+				}
+
+				old.Command = dockerInspect[0].Spec.TaskTemplate.ContainerSpec.Args
+				old.Image = strings.Split(dockerInspect[0].Spec.TaskTemplate.ContainerSpec.Image, "@")[0]
+				old.Init = dockerInspect[0].Spec.TaskTemplate.ContainerSpec.Init
+				old.Networks = networksExisting
+				old.Publish = portsExisting
+				old.Replicas = fmt.Sprint(dockerInspect[0].Spec.Mode.Replicated.Replicas)
+				old.Secrets = secretsExisting
+				if dockerInspect[0].Spec.UpdateConfig.Delay != 0 {
+					delayNs, _ := time.ParseDuration(fmt.Sprint(dockerInspect[0].Spec.UpdateConfig.Delay, "ns"))
+					old.UpdateDelay, _ = strings.CutSuffix(delayNs.String(), "m0s")
+				}
+				old.UpdateFailureAction = dockerInspect[0].Spec.UpdateConfig.FailureAction
+				if old.UpdateFailureAction == "pause" {
+					old.UpdateFailureAction = ""
+				}
+				old.UpdateOrder = dockerInspect[0].Spec.UpdateConfig.Order
+				if old.UpdateOrder == "stop-first" {
+					old.UpdateOrder = ""
+				}
+				old.UpdateParallelism = fmt.Sprint(dockerInspect[0].Spec.UpdateConfig.Parallelism)
+				if old.UpdateParallelism == "1" {
+					old.UpdateParallelism = ""
+				}
+				old.User = dockerInspect[0].Spec.TaskTemplate.ContainerSpec.User
+				old.WorkDir = dockerInspect[0].Spec.TaskTemplate.ContainerSpec.Dir
+
+				diffText, _ := old.Diff(old)
+				fmt.Printf("\nCurrent state of %s:\n\n", cmd.Name)
+				fmt.Printf("   service %s:\n", cmd.Name)
+				fmt.Print(diffText, "\n\n")
 			}
-			if len(dockerInspect) < 1 {
-				return fmt.Errorf("empty docker service inspect JSON: %s", dockerInspect)
-			}
-			out, err := json.MarshalIndent(dockerInspect[0], "", "    ")
-			if err != nil {
-				fmt.Println("🚫 Could not format JSON:\n", dockerInspect[0])
-				return err
-			}
-			fmt.Println(string(out))
 			return nil
 		}).
 		OnError(func(err error) error {
--- inspect_test.go
+++ inspect_test.go
@@ -7,6 +7,36 @@ import (
 
 func TestInspectCommand(t *testing.T) {
 	if err := testDatabase(func() error {
+		mock := &SshConnectionMock{Result: `[{"ID":"fake-service-id","Spec":{"Name":"files","Labels":{"rove":"service"},"Mode":{"Replicated":{"Replicas":1}},"UpdateConfig":{"Parallelism":1,"FailureAction":"pause","Monitor":5000000000,"MaxFailureRatio":0,"Order":"stop-first"},"TaskTemplate":{"ContainerSpec":{"Image":"python:3.12@sha256:05855f5bf06f5a004b0c1a8aaac73a9d9ea54390fc289d3e80ef52c4f90d5585","Args":["python3","-m","http.server","80"],"Init":false,"StopGracePeriod":10000000000,"DNSConfig":{},"Isolation":"default"}}},"Version":{"Index":1234}}]`}
+		expectedCmd := []string{"docker service inspect --format json fake-service"}
+		expected := "\n" + `Current state of fake-service:
+
+   service fake-service:
+     command  = ["python3","-m","http.server","80"]
+     image    = "python:3.12"
+     replicas = "1"` + "\n\n"
+
+		capture(t).
+			Run(func() error {
+				cmd := &InspectCommand{
+					Machine: "default",
+					Name:    "fake-service",
+				}
+				return cmd.Do(mock, nil)
+			}).
+			ExpectStdout(expected)
+
+		if !slices.Equal(mock.CommandsRun, expectedCmd) {
+			t.Errorf("'%#v' did not match expected.", mock.CommandsRun)
+		}
+		return nil
+	}); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestInspectCommandJson(t *testing.T) {
+	if err := testDatabase(func() error {
 		mock := &SshConnectionMock{Result: `[{"ID":"fake-service-id","Version":{"Index":1234}}]`}
 		expectedCmd := []string{"docker service inspect --format json fake-service"}
 		expected := `{
@@ -21,6 +51,7 @@ func TestInspectCommand(t *testing.T) {
 				cmd := &InspectCommand{
 					Machine: "default",
 					Name:    "fake-service",
+					Json:    true,
 				}
 				return cmd.Do(mock, nil)
 			}).
